55228c792305a505338add641f234a66e48c11cf
DinDin <din.din@isunCloud.com>
2018-10-03 18:38:57 +0800

    [Add] add  API Manager & provider setting

--- TaipeiZooDemo/APIService.swift
+++ TaipeiZooDemo/APIService.swift
@@ -10,8 +10,8 @@ import Foundation
 enum APIError: String, Error {
     case parsingError
     case statusError
+    case loading
 }
-
 
 enum HTTPMethod: String {
     case GET
@@ -28,12 +28,12 @@ protocol SessionProcotol {
 }
 
 protocol ProviderProcotol {
-    func getList(page: Int, completionHandler: @escaping ProviderCompletionHandler)
+    func getList(limit: Int, offset: Int, completionHandler: @escaping ProviderCompletionHandler)
 }
 
 class Session: SessionProcotol {
     
-    func requestData(url: URL, method: HTTPMethod, parameters: Parameters? = nil, header: HTTPHeaders? = nil ,completionHandler: @escaping ProviderCompletionHandler) {
+    func requestData(url: URL, method: HTTPMethod, parameters: Parameters? = nil, header: HTTPHeaders? = nil, completionHandler: @escaping ProviderCompletionHandler) {
         var request = URLRequest(url: url)
         request.httpMethod = method.rawValue
         request.allHTTPHeaderFields = header
@@ -47,34 +47,69 @@ class Session: SessionProcotol {
 class Provider: ProviderProcotol  {
     
     private var domainString: String = ""
-    private var sesion = Session()
-    
-    init(domainString: String) {
+    private var session: SessionProcotol!
+    init(domainString: String, session: SessionProcotol? = nil ) {
         self.domainString = domainString
+        self.session = session != nil ? session! : Session() 
     }
     
     //MARK: GET Method
-    func getList(page: Int, completionHandler: @escaping ProviderCompletionHandler) {
-        guard let url = URL(string: "") else {
+    func getList(limit: Int, offset: Int, completionHandler: @escaping ProviderCompletionHandler) {
+        var components = URLComponents()
+        var scheme = "https://"
+        var host = domainString
+        if domainString.contains("://") {
+            let list = domainString.components(separatedBy: "://")
+            if let first = list.first {
+                scheme = first
+            }
+            if let last = list.last {
+                host = last
+            }
+        }
+        components.scheme = scheme
+        components.host = host
+        components.path = "/opendata/datalist/apiAccess"
+        let queryItemScope = URLQueryItem(name: "scope", value: "resourceAquire")
+        let queryItemRid = URLQueryItem(name: "rid", value: "a3e2b221-75e0-45c1-8f97-75acbd43d613")
+        let queryItemLimit = URLQueryItem(name: "limit", value: String(limit))
+        let queryItemOffset = URLQueryItem(name: "offset", value: String(offset))
+        components.queryItems = [queryItemScope, queryItemRid, queryItemLimit, queryItemOffset ]
+        guard let url =  components.url else {
             return
         }
-        sesion.requestData(url: url , method: .GET, parameters: nil, header: nil, completionHandler: completionHandler)
+        session.requestData(url: url , method: .GET, parameters: nil, header: nil, completionHandler: completionHandler)
     }
-    
 }
 
+//https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=a3e2b221-75e0-45c1-8f97-75acbd43d613&limit=3
 
 class APIManager {
-    
     static let shared = APIManager()
-    private var provider: Provider?
-    
-    func setProvider(session: ProviderProcotol? = nil, urlString: String) {
-        self.provider = Provider(domainString: urlString)
+    private var provider: ProviderProcotol?
+    func setProvider(provider: ProviderProcotol? = nil, urlString: String) {
+        if let _provider =  provider {
+            self.provider = _provider
+        } else {
+            self.provider = Provider(domainString: urlString)
+        }
     }
     
-    func getSearchRequest( page: Int = 0, completion: @escaping (Data?, Error?) -> Void) {
-        provider?.getList(page: page, completionHandler: { (data, response, error) in
+    func getSearchRequest( limit: Int = 0, offset: Int = 0, completion: @escaping (ResutlRoot?, Error?) -> Void) {
+
+        provider?.getList(limit: limit, offset: offset, completionHandler: { (data, response, error) in
+            guard let data = data else {
+                completion(nil, APIError.parsingError)
+                return 
+            }
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            do {
+                let result = try decoder.decode(ResutlRoot.self, from: data)
+                completion(result, nil)
+            } catch {
+                completion(nil, APIError.parsingError)
+            }
         })
     }
 }
--- TaipeiZooDemo/AppDelegate.swift
+++ TaipeiZooDemo/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        APIManager.shared.setProvider(urlString: "https://data.taipei")
         return true
     }
 
