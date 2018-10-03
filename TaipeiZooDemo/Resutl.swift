c2a1990c99b692d7e16d194e375cf1fa33d792d8
DinDin <din.din@isunCloud.com>
2018-10-03 18:37:46 +0800

    [Add] add model

--- TaipeiZooDemo/Resutl.swift
+++ TaipeiZooDemo/Resutl.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+struct Result : Codable {
+    
+    let aBehavior: String?
+    let aLocation: String?
+    let aNameCh: String?
+    let aPic01URL: String?
+    let aInterpretation: String?
+ 
+    enum CodingKeys: String, CodingKey {
+   
+        case aBehavior = "A_Behavior"
+        case aLocation = "A_Location"
+        case aNameCh = "A_Name_Ch"
+        case aPic01URL = "A_Pic01_URL"
+        case aInterpretation = "A_Interpretation"
+    }
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        aBehavior = try values.decodeIfPresent(String.self, forKey: .aBehavior)
+        aLocation = try values.decodeIfPresent(String.self, forKey: .aLocation)
+        aNameCh = try values.decodeIfPresent(String.self, forKey: .aNameCh)
+        aPic01URL = try values.decodeIfPresent(String.self, forKey: .aPic01URL)
+        aInterpretation = try values.decodeIfPresent(String.self, forKey: .aInterpretation)
+
+    }
+    
+    
+}
--- TaipeiZooDemo/ResutlRoot.swift
+++ TaipeiZooDemo/ResutlRoot.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+struct ResutlRoot: Codable {
+    let count : Int?
+    let limit : Int?
+    let offset : Int?
+    let results : [Result]?
+    let sort : String?
+    
+    enum CodingKeys: String, CodingKey {
+        case count
+        case limit
+        case offset
+        case results
+        case sort
+    }
+    
+    enum RootKeys: String, CodingKey {
+        case result
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: RootKeys.self)
+        let values = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .result)
+        count = try values.decodeIfPresent(Int.self, forKey: .count)
+        limit = try values.decodeIfPresent(Int.self, forKey: .limit)
+        offset = try values.decodeIfPresent(Int.self, forKey: .offset)
+        results = try values.decodeIfPresent([Result].self, forKey: .results)
+        sort = try values.decodeIfPresent(String.self, forKey: .sort)
+    }
+    
+}
